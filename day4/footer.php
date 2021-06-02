61d9f2c40373609dae69dd883f64ae5bb7965d97
amishadotcom <84851707+amishadotcom@users.noreply.github.com>
2021-06-02 18:58:59 +0530

    day4

---
+++ day4/footer.php
@@ -0,0 +1,24 @@
+<footer>
+        <div class="footer layout_padding">
+            <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <h3>Contact Us</h3>
+                            <p>A/30 hriji park scoiety,Ahmedabad<br><br>9714517548<br><br>amishapatel697@gmail.com</p>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <h3>Pages</h3>
+                            <p>
+                              <a href="#home">Home</a><br>
+                              <a href="#about">About Us</a><br>
+                              <a href="#fruits">Fruits</a><br>
+                              
+                              <a href="#contact">Contact Us</a>
+                        </div>
+                        
+        <div class="copyright">
+            <div class="container">
+                <p>This website is managed by Amisha Patel</p>
+            </div>
+        </div>
+    </footer>
---
+++ day4/sliderbar.php
@@ -0,0 +1,18 @@
+<section class="slider_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="full">
+                        <h1><strong class="red">Amisha</strong><br>Fresh Apple shop</h1>
+                            
+                            <div class="button_section"><a class="main_bt" href="#">Buy Now</a></div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="full text_align_center">
+                        <img class="slide_img" src="images/slider_img.png" alt="#" /> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
