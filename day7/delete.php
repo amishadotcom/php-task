8151db90af1c75a274e1e376876d0466a2e6442b
amishadotcom <84851707+amishadotcom@users.noreply.github.com>
2021-06-02 19:05:07 +0530

    Add files via upload

--- day7/delete.php
+++ day7/delete.php
@@ -1,1 +1,14 @@
+<?php
+$connection = mysqli_connect("localhost","root","","demo");
+ $id = $_GET['deleteid'];
+
+ $q=mysqli_query($connection,"update user set is_delete = 1 where id='{$id}'") 
+ or die("Error".mysqli_error($connection));
+
+ if($q)
+ {
+ 	echo"<script>alert('record delete');
+ 	window.location='display.php'</script>";
+ }
+ ?>
 
---
+++ day7/display.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>display data</title>
+</head>
+<body>
+
+
+
+<?php
+$connection = mysqli_connect("localhost","root","","demo");
+$q=mysqli_query($connection,"select * from user where is_delete=0")
+or die("Error".mysqli_error($connection));
+
+echo "<table border='1'>";
+echo "<tr>";
+echo "<th>id</th>";
+echo "<th>name</th>";
+echo "<th>gender</th>";
+echo "<th>mobile</th>";
+echo "<th>Action</th>";
+
+$i=0;
+while($row = mysqli_fetch_array($q))
+ {
+	$i++;
+	echo "<tr>";
+	echo "<td>$i</td>";
+	echo "<td>{$row['user_name']}</td>";
+	echo "<td>{$row['user_gender']}</td>";
+	echo "<td>{$row['user_mobile']}</td>";
+	echo "<td><a href='delete.php?deleteid={$row['id']}'>Delete</a></td>";
+	echo "</tr>";
+}
+echo "</table>";
+echo "<a href='user_registration.php'>Add record</a>";
+?>
+
+</body>
+</html>
